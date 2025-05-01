fe2910a495579a9c601befcdac78d9e5ff776800
rahulsantosh <rahulsantosh9999@gmail.com>
2025-05-01 16:15:40 +0530

    create subnet

--- tf-aws/vpc.tf
+++ tf-aws/vpc.tf
@@ -1,8 +1,19 @@
+#aws vpc
 resource "aws_vpc" "test" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "test"
+  }
+}
+
+#web subnet
+resource "aws_subnet" "test-web-sn" {
+  vpc_id     = aws_vpc.test.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "test-web-subnet"
   }
 }
